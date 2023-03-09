1c4438b0e001ce8d63bc16fd46f8c3cb12bc13b0
ac0049 <39913339+ac0049@users.noreply.github.com>
2023-03-09 16:16:51 -0500

    Update UrlParameters.js

--- UrlParameters.js
+++ UrlParameters.js
@@ -1,3 +1,11 @@
+// First way to do that
+var text = 'http://localhost/mysite/includes/phpThumb.php?src=http://media2.jupix.co.uk/v3/clients/4/properties/795/IMG_795_1_large.jpg&w=592&aoe=1&q=100';
+var newSrc = 'www.google.com';
+var newText = text.replace(/(src=).*?(&)/,'$1' + newSrc + '$2');
+
+
+
+// Second
 // Construct URLSearchParams object instance from current URL querystring.
 var queryParams = new URLSearchParams(window.location.search);
  
